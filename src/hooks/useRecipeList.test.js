f30f60d2f9001197dff6365192737c82bbd181ee
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2020-06-03 17:52:21 +0300

    fix test

--- src/hooks/useRecipeList.test.js
+++ src/hooks/useRecipeList.test.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import { Provider, useDispatch } from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
+import configureStore from 'redux-mock-store';
 import useRecipeList from './useRecipeList';
-import { getList } from '../services/recipeService';
+import getList from '../services/recipeService';
 // import * as recipeService from '../services/recipeService';
 import recipes from '../__mocks__/recipesData';
 import user from '../__mocks__/userData';
+import store from '../store/store';
 
 jest.mock('../services/recipeService');
 
-const store = configureStore()({ recipes: [], user });
+//
 
 describe('useRecipesList hook', () => {
-  afterAll(() => {
+  afterEach(() => {
     jest.resetModules();
+    jest.clearAllMocks();
   });
-
-  const wrapper = ({ children }) => <Provider store={store}>{children}</Provider>;
 
   test('should fetch recipes from firestore', async () => {
     // Arrange
-    getList.mockResolvedValue(recipes);
+    const mockDispath = jest.fn();
+    const wrapper = ({ children }) => (
+      <Provider store={store} dispath={mockDispath}>
+        {children}
+      </Provider>
+    );
+    getList.mockImplementation(() => Promise.resolve(recipes));
 
     // Act
     const { result, waitForNextUpdate } = renderHook(() => useRecipeList(), { wrapper });
@@ -31,26 +37,21 @@ describe('useRecipesList hook', () => {
     await waitForNextUpdate();
     expect(getList).toHaveBeenCalled();
     expect(result.current.loading).toEqual(false);
-    // expect(result.current.recipeList).toEqual(recipes);
+    expect(result.current.recipeList).toEqual(recipes);
   });
 
-  // test('should fetch with error', async () => {
-  //   // Arrange
-  //   const expectedError = new Error('Some firestore error');
-  //   const mockGetList = jest.fn(() => Promise.reject(expectedError));
-  //   getList.mockImplementation(mockGetList);
-  //   let actualError;
+  test('should fetch with error', async () => {
+    // Arrange
+    const mockStore = configureStore()({ recipes: [], user });
+    const wrapper = ({ children }) => <Provider store={mockStore}>{children}</Provider>;
+    const expectedError = new Error('Some firestore error');
+    const mockGetList = jest.fn(() => Promise.reject(expectedError));
+    getList.mockImplementation(mockGetList);
 
-  //   // Act
-  //   const { waitForNextUpdate } = renderHook(() => useRecipeList(), { wrapper });
-
-  //   try {
-  //     await waitForNextUpdate();
-  //   } catch (e) {
-  //     actualError = e;
-  //     console.log('error');
-  //   }
-  //   // Assert
-  //   expect(actualError).toEqual(expectedError);
-  // });
+    // Act
+    const { result } = renderHook(() => useRecipeList(), { wrapper });
+    // Assert
+    expect(result.current.loading).toEqual(false);
+    expect(result.current.recipeList).toEqual([]);
+  });
 });
